5a918159344ba7824caf624c41eb9032b758389b
Claire Stranack <claire@stranack.co.uk>
2020-03-23 12:22:22 +0000

    handlebars

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
 var express = require('express');
 var app = express();
+var handlebars = require('express-handlebars');
+
+app.set('view engine', 'hbs');
+app.engine('hbs', handlebars({
+    layoutsDir: __dirname + '/views/layouts',
+    extname: 'hbs'
+}));
+   
 
 //web application getting something from the server
 //req = request //res = response //=>'fat arrow' = function
 app.get('/', (req, res) => {
-    res.send('This is the homepage :D ');
+    res.render('index', { layout: 'main'});
 
 });
 //second page
 app.get('/about', (req, res) => {
-    res.send('This is the about page :D ');
-
-});
-
-//third page
-app.get('/contact', (req, res) => {
-    res.send('This is the contact page :D ');
+    res.render('about', { layout: 'main'});
 
 });
 
